b9d10475ed8784337712150c3223b10b01e55a01
Youssef Achenchabe <youssef.achenchabe@gmail.com>
2017-12-28 21:36:17 +0100

    serv

--- src/pack/Serv.java
+++ src/pack/Serv.java
@@ -2,6 +2,8 @@ package pack;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
@@ -43,22 +45,43 @@ public class Serv extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
+		
+		String operation = request.getParameter("operation");
+		PrintWriter out = response.getWriter();
+		switch (operation) {
+			case "connexion":
+				
+		        
+		        String email = request.getParameter("email");
+		        String pass = request.getParameter("pass");
+		        
+		        if(ValidationUser.verifierUser(f, email, pass)) {
+		            RequestDispatcher rs = request.getRequestDispatcher("Bienvenue");
+		            rs.forward(request, response);
+		        } else {
+		           out.println("Adresse ou mdp incorrects");
+		           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+		           rs.include(request, response);
+		        }
+		        break;
+		        
+			case "inscription":
+				
+				String nom = request.getParameter("nom");
+				String prenom = request.getParameter("prenom");
+				String mdp = request.getParameter("mdp");
+				String adresse = request.getParameter("adresse");
+				String adresseMail = request.getParameter("email");
+				Boolean isClient = true;
+				List<String> gouts = new ArrayList<String>();
+				if (request.getParameter("isClient").equals("false")) {
+					isClient = false;
+				}
+				f.addClient(prenom, nom, mdp, adresseMail, adresse, gouts, isClient);
+				request.getRequestDispatcher("connexion.html").forward(request, response);
+				break;
+		}
         
-        String email = request.getParameter("email");
-        String pass = request.getParameter("pass");
-        
-        if(ValidationUser.verifierUser(f, email, pass))
-        {
-            RequestDispatcher rs = request.getRequestDispatcher("Bienvenue");
-            rs.forward(request, response);
-        }
-        else
-        {
-           out.println("Adresse ou mdp incorrects");
-           RequestDispatcher rs = request.getRequestDispatcher("index.html");
-           rs.include(request, response);
-        }
 	}
 
 }
